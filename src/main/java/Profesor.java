1e8c15c4abb112e9d17f96d2bd63a86a601a4e45
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-06-01 16:44:02 -0500

    En las líneas 5 y 6 se crea el constructor de la clase Profesor incluyendo parámetros de la clase padre y nuevos.

--- src/main/java/Profesor.java
+++ src/main/java/Profesor.java
@@ -1,3 +1,6 @@
 public class Profesor extends Persona {
     private String especialidad;
     private String titulo;
+
+    public Profesor(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso,
+                    String especialidad, String titulo) {
