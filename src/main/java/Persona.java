d96dcd75bef06521c1697d3a3e2c59d9be344ce5
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-05-31 22:32:00 -0500

    En la línea de la 2 a la 7 se declaran los atributos protegidos de la clase Persona: nombre, apellido, fecha de nacimiento, género, estatura y peso.

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,1 +1,7 @@
 public class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected String fechaNacimiento;
+    protected String genero;
+    protected double estatura;
+    protected double peso;
