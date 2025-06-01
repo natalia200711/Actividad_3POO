a8a2f8513970ae4b14c61489945c9390d25a85d5
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-05-31 22:35:32 -0500

    En la línea 9 se implementa el constructor de la clase Persona con parámetros para todos los atributos.

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -5,3 +5,5 @@ public class Persona {
     protected String genero;
     protected double estatura;
     protected double peso;
+
+    public Persona(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso) {
