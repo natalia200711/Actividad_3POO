d5427e3dcef0fa986c6abf7d6048e3086be7876d
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-06-04 15:22:44 -0500

    En las líneas de la 10 a la 13 se crea método getConnection() y se carga el driver JDBC

--- src/main/java/ConexionDB.java
+++ src/main/java/ConexionDB.java
@@ -6,3 +6,8 @@ public class ConexionDB {
     private static final String URL = "jdbc:mysql://localhost:3306/registro_profesores?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "naty1101";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            // Cargar el driver JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
