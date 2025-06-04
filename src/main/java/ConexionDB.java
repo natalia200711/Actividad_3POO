16af2d8c0eb05879242c1050214f30c51c8c1390
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-06-04 15:19:00 -0500

    En las líneas de la 5 a la 8 Se crea la clase ConexionDB y se definen las constantes necesarias para conectarse a la base de datos (URL, usuario y contraseña).

--- src/main/java/ConexionDB.java
+++ src/main/java/ConexionDB.java
@@ -1,3 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/registro_profesores?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "naty1101";
