14df7dcd1e22ba14c6c10d2f933223ce0e6b4235
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-06-04 15:37:02 -0500

    Correción de la línea 6

--- src/main/java/ConexionDB.java
+++ src/main/java/ConexionDB.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    private static final String URL = "jdbc:mysql://localhost:3306/registro_profesores?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/registro_profesores";
     private static final String USER = "root";
     private static final String PASSWORD = "naty1101";
 
