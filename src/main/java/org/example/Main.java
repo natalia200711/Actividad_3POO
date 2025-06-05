5ab91f22fe57a111cf19a09ccec17fe74e910b5f
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-06-05 16:45:02 -0500

    En la línea 38 se agregó el consumo del salto de línea generado por nextDouble para evitar errores en las siguientes capturas de texto.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,3 +23,5 @@ public class Main {
 
         System.out.println("Ingrese el peso del profesor (en kilogramos):");
         double peso = scanner.nextDouble();
+
+        scanner.nextLine(); // Consumir el salto de línea
